e4fee0632dc4cb0abad44d36a6a96e6d4ed90a42
Ross Hadden <rosshadden@gmail.com>
2017-12-18 00:44:03 -0500

    Updates.

--- scripts/build.sh
+++ scripts/build.sh
@@ -4,7 +4,12 @@ format=${1:-tex_pdf}
 
 cd dist || exit
 
-json_resume convert --out="$format" --template=../templates/pdf.mustache ../rosshadden.json
+json_resume convert --out="$format" ../rosshadden.json
 if [ -f resume.pdf ]; then mv resume.pdf rosshadden.pdf; fi
 
 cd .. || exit
+
+# ext=${1:-pdf}
+# theme=${2:-paper}
+#
+# resume export -t "$theme" "dist/rosshadden.$ext"
